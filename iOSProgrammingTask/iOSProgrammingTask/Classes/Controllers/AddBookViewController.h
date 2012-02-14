0adaac847aeed9f01ecf56909b7b9df63ee74f08
Omer <janjuaomer@gmail.com>
2012-02-14 16:22:00 +0000

    files added for task 7

---
+++ iOSProgrammingTask/iOSProgrammingTask/Classes/Controllers/AddBookViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddBookViewController.h
+//  iOSProgrammingTask
+//
+//  Created by Omer Janjua on 14/02/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddBookViewController : UITableViewController
+
+@end
